c0be47b20370c4b6fe09168759de8402b7e1b0c7
dassin256 <d.lee.co.ltd@gmail.com>
2022-12-03 12:10:23 +0800

    Initial commit

---
+++ app/component/MessageComponent.js
@@ -0,0 +1,42 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { Ionicons } from "@expo/vector-icons";
+import { styles } from "../utils/styles";
+
+export default function MessageComponent({ item, user }) {
+	const status = item.user !== user;
+
+	return (
+		<View>
+			<View
+				style={
+					status
+						? styles.mmessageWrapper
+						: [styles.mmessageWrapper, { alignItems: "flex-end" }]
+				}
+			>
+				<View style={{ flexDirection: "row", alignItems: "center" }}>
+					<View>
+						<Ionicons
+							name='person-circle-outline'
+							size={30}
+							color='black'
+							style={styles.mavatar}
+						/>
+						<Text style={styles.messenger}>{item.user}</Text>
+					</View>
+					<View
+						style={
+							status
+								? styles.mmessage
+								: [styles.mmessage, { backgroundColor: "rgb(194, 243, 194)" }]
+						}
+					>
+						<Text>{item.text}</Text>
+					</View>
+				</View>
+					<Text>{item.time}</Text>
+			</View>
+		</View>
+	);
+}
